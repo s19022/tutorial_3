abe0790e4f420f6794a0e94b6e0fa339229f0344
Volodymyr Dovhyi <s19022@pjwstk.edu.pl>
2020-04-03 16:00:25 +0300

    Task_1

---
+++ Tutorial_3/Controllers/EnrollmentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorial_3.Controllers
+{
+    [Route("api/enrollment")]
+    [ApiController]
+    public class EnrollmentController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddStudent(StudentController student)
+        {
+            return Ok("fff");
+        }
+    }
+}
--- Tutorial_3/Models/Student.cs
+++ Tutorial_3/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace Tutorial_3.Models
 {
     public class Student
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        [Required]
         public string IndexNumber { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        public string BirthDate { get; set; }
+        
+        public string Studies { get; set; }
 
 
     }
